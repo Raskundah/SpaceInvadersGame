e0805acbc34481a9e15ae919bfda5916cb1b337c
Raskundah <100294321+Raskundah@users.noreply.github.com>
2022-03-31 12:30:43 +0100

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceInvadersGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D alienTexture;
+        private Texture2D bulletTexture;
+        private Texture2D playerTexture;
+        private Texture2D shieldTexture;
+        private SpriteFont gameFont;
+
+        Player player = new Player();
+        Controller gameController = new Controller();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            alienTexture = Content.Load<Texture2D>("alien");
+            bulletTexture = Content.Load<Texture2D>("bullet");
+            playerTexture = Content.Load<Texture2D>("player");
+            shieldTexture = Content.Load<Texture2D>("shield");
+            gameFont = Content.Load<SpriteFont>("gameFont");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceInvadersGame
+{
+    class Player
+    {
+        int speed = 500;
+    }
+}
