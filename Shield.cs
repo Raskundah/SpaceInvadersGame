54bb3ee58c17994d43ca55b310f57b291669a4ca
Raskundah <100294321+Raskundah@users.noreply.github.com>
2022-03-31 12:39:36 +0100

    0.1
    added idea

---
+++ Shield.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceInvadersGame
+{
+    // for space invader shields, make them comprised of many small individual sections, that disappear on hit.
+    class Shield
+    {
+    }
+}
