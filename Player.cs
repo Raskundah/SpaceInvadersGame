f3d2036844e6d2b287f3f030301db8c13819ffe6
Raskundah <100294321+Raskundah@users.noreply.github.com>
2022-03-31 12:59:00 +0100

    Update Player.cs
    working on player render and movement.

--- Player.cs
+++ Player.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace SpaceInvadersGame
 {
     class Player
+
     {
-        int speed = 500;
+        private GraphicsDeviceManager _graphics;
+        public Vector2 playerPosition = new Vector2();       
+
+        public Player(GraphicsDeviceManager graphics)
+        {
+            _graphics = graphics;
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 50);
+        }
+
+        public void PlayerUpdate(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+
+
+        }
+
+
+
+         
     }
 }
